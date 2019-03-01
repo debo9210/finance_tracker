537641588876274c2c27f0747140e475ab036e62
debo9210 <omololuadebowale1@gmail.com>
2019-03-01 11:59:04 +0200

    Add users search display and friends functionality

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   resources :friendships
   get 'my_friends', to: 'users#my_friends'
   get 'search_friends', to: 'users#search'
+  post 'add_friend', to: 'users#add_friend'
 
 end
